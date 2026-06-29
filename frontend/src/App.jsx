7999473edba1ed805a335f6a69c5ebb8f28b196a
Samuel Henrique <samu.hwf@gmail.com>
2026-06-29 10:19:42 -0300

    criação da tela principal/headers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import PrivateRoute from './routes/PrivateRoute'
+import AppLayout from './layouts/AppLayout'
 
 export default function App() {
   return (
@@ -9,7 +10,13 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route 
+          element={<PrivateRoute>
+            <AppLayout />
+          </PrivateRoute>
+          }>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
       </Routes>
     </BrowserRouter>
   )
